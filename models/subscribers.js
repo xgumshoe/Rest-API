72b155c50954aff8d85a5fff99b36a017a03c17f
xgumshoe <narljohn@gmail.com>
2020-12-09 14:06:48 +0800

    Initial commit

---
+++ models/subscribers.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const subscribersSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    subscribedToChannel: {
+        type: String,
+        required: true
+    },
+    subscribeData: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('Subscriber', subscribersSchema)
